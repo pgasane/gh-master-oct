d5a47f900d8c16462183888244d9c2670ee6bcf2
Your Name <you@example.com>
2022-10-09 11:07:46 +0000

    Declaración de dependencias

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -1,7 +1,13 @@
 """
 Este módulo contiene helpers para el preprocesamiento de datos
+La idea es tener herramientas que no estén en sklearn a disposición de la empresa
 """
 import numpy as np
 
+'''
+Devuelve todas las columnas numéricas de un DataFrame
+Entrada DataFrame
+Salida Lista con las columnas numéricas del DataFrame de entrada
+'''
 def get_numerical_features(df):
     return list(df.select_dtypes(include=[np.number]).columns)
